ff03cea20d08723591fe9eae420738631bd5a3f3
GrahamboJangles <36944031+GrahamboJangles@users.noreply.github.com>
2021-04-20 15:34:11 -0400

    Update PutPremiumProcessor.py

--- PutPremiumProcessor.py
+++ PutPremiumProcessor.py
@@ -1,27 +1,22 @@
+# from wallstreet import Put, Stock
 import wallstreet
 from wallstreet import *
 import pandas as pd 
- 
+
 global return_per_year_target
 return_per_year_target = 25
- 
-# ticker = 'CVM'
 
 """
 Tickers are currently passed not as a list, but as a string of tickers separated by spaces and/or tabs.
 This is because it's easier to copy and past a list of tickers found from a table on the internet.
+https://www.barchart.com/options/iv-rank-percentile/stocks?viewName=main&orderBy=optionsImpliedVolatilityRank1y&orderDir=desc
 """
-# tickers = ['CVM', 'CCIV', 'CLOV', 'CAN', 'MVIS', 'WBAI', 'BTBT', 'FCEL', 'GME', 'VSPR', 'MARA', 'EBIX'] 
-tickers = 'CVM CCIV CLOV CAN MVIS WBAI BTBT FCEL GME VSPR MARA EBIX'
-tickers += ' '
-tickers += 'CVM  YY  TME BLOK    MJ  APHA    XNET    PDD BILI    HUYA    MUX SSYS    MOMO    DBX TDC GILT    TLRY    ZTO BIDU    VKTX    NRG AGTC    XXII    QTT MRVL    TXMD'
-tickers += ' '
-tickers += 'SNDL    VZ  AMD QS  GSAT    MSFT    WMT BP  AAL TWTR    MARA    DNN F   FB  BABA    BAC UBER    VALE    CCIV    TLRY    GME GOLD    BA  BB  NVDA    C   AMC NCLH    FCX DIS FUTU    CBAT    XOM RIOT    CVS EBON    SQ  FUBO    NOK WBA PSTH    GE  XPEV    FSR X   DKNG    PLUG    INTC    ET  APHA    JD  FSLY    PFE BIDU    BLNK    FCEL    PTON    NXTD    WKHS    LODE    ZM  NNDM    CVX LI  RKT SPCE    NFLX    ACB LVS SBUX    PYPL    SENS    QCOM    GM  BNGO    MU  CRM MRK JPM IDEX    SHOP    AR  CSCO    SOS CLOV    TIGR    TGT MRVL    GSX UAL MGM MVIS'
+
+tickers = 'ESGC	HAE	GSX	IAC	RBLX	PLBY	OUST	LFMD	CLOV	MSTR	SNAP	TWLO	IIVI	FTCH	SPOT	RIDE	TWTR	FEYE	X	DOYU	AMKR	TROX	ETSY	Z	KTOS	PINS	INTC	EA	RTX	TAL	FNKO	INSG	PYPL	ARVL	TIGR	FSR	FSLR	DDOG	FB	VIAC	YETI	GIK	XXII	AGC	CVM	BTI	GOOG	SPWR	IQ	GOOGL	CSCO	DASH	NVTA	HUYA	UPS	AMAT	NVAX	ADI	STX	LOW	ENPH	CZR	LAC	SI	HBI	EBAY	GOEV	ATVI	BIDU	WDC	CLF	RVLV	ISRG	ROKU	SONO	WMT	BAX	TGT	DNMR	ASXC	ANGI	CLNE	ASML	BHC	AMD	RMO	UAA	BILI	BRK.B	W	TME	SQ	NUE	BYND	JMIA	SPLK	PSFE	FSLY	VIPS	KSU'
 # tickers = tickers.split('   ')
 # tickers = tickers.split('  ')
-tickers = tickers.split(' ')
-
-# tickers = ['CVM']
+tickers = tickers.split('\t')
+# tickers = tickers.split(' ')
  
 #https://stackoverflow.com/questions/1801668/convert-a-python-list-with-strings-all-to-lowercase-or-uppercase
 # Capitalize tickers
@@ -42,26 +37,53 @@ scores = []
 strikes_list = []
 expiration_list = []
 tickers_list = []
-return_per_day_list = []
+# return_per_day_list = []
 return_per_year_list = []
 option_prices_list = []
 underlying_price_list = []
+option_volume_list = []
+last_underlying = ""
+last_average_pct_change = 0
  
 def getdata(option):
   print(f"\t {option}")
  
-  if option.volume == 0:
-    print("No option volume")
-    return scores, strikes_list, expiration_list
+  if option.volume <= 20:
+    print('Option volume <= 20')
+    return scores, strikes_list, expiration_list, tickers_list, return_per_year_list, option_prices_list, underlying_price_list, option_volume_list
+  
+ 
+  #print(f"Current put price: {option.price}")
+ 
+  # if option.bid == 0:
+  #   bid = 0.01
+  # else: bid = option.bid
+  # mid = (bid + option.ask) / 2
+ 
+  # if option.price < mid:
+  #   price = option.price
+  # elif option.volume <= 20:
+  #   price = option.bid
+  # else: price = mid
  
   price = option.bid
   if price == 0:
     # price = 0.01
     print('Price is 0')
-    return scores, strikes_list, expiration_list
+    return scores, strikes_list, expiration_list, tickers_list, return_per_year_list, option_prices_list, underlying_price_list, option_volume_list
   price = price * 100
-    
+ 
+  #print(f"Current put bid price: ${bid}")
+ 
+  #print(f"Current implied_volatility: {option.implied_volatility()}")
+ 
+  #print(f"Current delta: {option.delta():.5f}")
+  # probability = (1 - option.delta())*100
+  #print(f"Current probability: {probability:.2f}%")
   price_delta = option.underlying.price - option.strike
+  #print(f"Current vega: {option.vega()}")
+ 
+  #print(f"Current stock price: ${option.underlying.price}")
  
   from datetime import datetime
   from datetime import date
@@ -79,7 +101,7 @@ def getdata(option):
   if days_to_expiration == 0:
     # days_to_expiration = 1
     print("0 Days to expiration")
-    return scores, strikes_list, expiration_list
+    return scores, strikes_list, expiration_list, tickers_list, return_per_year_list, option_prices_list, underlying_price_list, option_volume_list
  
   collateral = option.strike * 100
   return_perc = ((collateral + price) - collateral) / collateral
@@ -90,16 +112,29 @@ def getdata(option):
   if return_per_year < return_per_year_target:
     print(f'Return per year: {return_per_day*365} does not meet {return_per_year_target}% target')
  
-    return scores, strikes_list, expiration_list
-  return_per_year_list.append(return_per_year)
+    return scores, strikes_list, expiration_list, tickers_list, return_per_year_list, option_prices_list, underlying_price_list, option_volume_list
+  
  
   #print(f"Return%: {return_perc} / price delta {price_delta}")
- 
-  score = (option.underlying.price / option.strike) / return_per_year
+  try: last_underlying
+  except: last_underlying = None
+  try: last_average_pct_change
+  except: last_average_pct_change = 0
+  if option.underlying == last_underlying:
+    average_pct_change = last_average_pct_change
+  else:
+    average_pct_change = option.underlying.historical(days_back=30, frequency='d')['Close'].pct_change().mean()
+  last_underlying = option.underlying
+  last_average_pct_change = average_pct_change
+  # score = ((option.underlying.price / option.strike) / return_per_year) / days_to_expiration
+  score = ((option.underlying.price / option.strike) / return_per_year)
+  # score = ((option.underlying.price / option.strike) / return_per_year) / days_to_expiration / abs(average_pct_change)
   # score2 = option.underlying.change / ((price_delta * return_per_day) / days_to_expiration)
   # score = (score1 + score2) / 2
   scores.append(score)
  
+  return_per_year_list.append(return_per_year)
+
   underlying_price_list.append(option.underlying.price)
  
   option_prices_list.append(price/100)
@@ -109,13 +144,15 @@ def getdata(option):
   strikes_list.append(option.strike)
  
   expiration_list.append(option.expiration)
+
+  option_volume_list.append(option.volume)
   # print(f"expiration_list: {expiration_list}")
   # print(f"strikes_list: {strikes_list}")
   # print(f"Scores: {scores}")
   #print(f"score: {score}")
   #print()
  
-  return scores, strikes_list, expiration_list
+  return scores, strikes_list, expiration_list, tickers_list, return_per_year_list, option_prices_list, underlying_price_list, option_volume_list
  
 years = []
 months = []
@@ -135,6 +172,13 @@ for ticker in tickers:
   except LookupError as e:
     print(f'Expirations LookupError: {e}')
     continue
+  except Exception as e:
+    print(f'Expirations Exception: {e}')
+    continue
+  
+  # Select only first four expirations
+  expirations = expirations[:4]
+
   # except Exction as e: 
   #   print(f"Error: {e}")
     # if "No options listed for given date" in str(e):
@@ -149,7 +193,8 @@ for ticker in tickers:
     month = int(split[1])
     year = int(split[2])
     try: strikes = wallstreet.Put(ticker, d=day, m=month, y=year).strikes
-    except LookupError as e: 
+    # except LookupError as e: 
+    except Exception as e:
       print(f'Strikes error: {e}')
       continue
     
@@ -164,27 +209,41 @@ for ticker in tickers:
     if i > 1:
       clear_output()  
  
-    print(f'Iterations: {i}')
+    print(f'Current Ticker: {ticker}')
+    print(f'Ticker iterations: {i}')
     print(f"Expirations: {expirations}")
     print(f"Strikes: {strikes}")
  
-    # Create the pandas DataFrame 
-    df = pd.DataFrame(
-                      {'scores': scores,
-                      'strikes': strikes_list,
-                      'expirations': expiration_list,
-                      'ticker': tickers_list,
-                      'return_per_year': return_per_year_list,
-                      'option prices': option_prices_list,
-                      'underlying price': underlying_price_list
-                      })
+    try:
+      # Create the pandas DataFrame 
+      df = pd.DataFrame(
+                        {'scores': scores,
+                        'strikes': strikes_list,
+                        'expirations': expiration_list,
+                        'ticker': tickers_list,
+                        'return_per_year': return_per_year_list,
+                        'option prices': option_prices_list,
+                        'underlying price': underlying_price_list,
+                        'option volume': option_volume_list
+                        })
+    except Exception as e:
+      print(e)
+      # print(scores, strikes, expirations, ticker, return_per_year, option prices)
+      # print(underlying price, option volume)
+
+      # for each in yup:
+      #   print(len(each))
+      #   print(each)
+        
+
+
  
     # dropping duplicate rows 
     df.drop_duplicates(keep=False, inplace=True) 
  
     # print dataframe. 
     sort_by_score = df.sort_values('scores', ascending=False)
-    display(sort_by_score.head(n=50))
+    display(sort_by_score.head(n=60))
  
     for strike in strikes:
       
@@ -196,7 +255,7 @@ for ticker in tickers:
         continue
       except Exception as e: print(e)
  
-      try: scores, strikes_list, expiration_list = getdata(option)
+      try: scores, strikes_list, expiration_list, tickers_list, return_per_year_list, option_prices_list, underlying_price_list, option_volume_list = getdata(option)
       except LookupError: 
         print("No options listed for this stock.")
         continue
